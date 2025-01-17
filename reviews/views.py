7cc85b84dbb5f12b4854060c19ce60ce6e6b5727
Esataydin <51443456+Esataydin@users.noreply.github.com>
2025-01-18 01:37:11 +0300

    views.py comment block fix

--- reviews/views.py
+++ reviews/views.py
@@ -29,32 +29,32 @@ class ReviewView(View):
         })
 
 
-def review(request):
-    if request.method == 'POST':
-        # existing_data = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=existing_data)
-        # form.save()
-        # Updating an instance in databse is possible by using ModelForm as shown above. 
-        # IT'S JUST A DEMONSTRATION, NOT VALID WITH GIVEN DATA IN THAT EXAPMLE
+# def review(request):
+#     if request.method == 'POST':
+#         # existing_data = Review.objects.get(pk=1)
+#         # form = ReviewForm(request.POST, instance=existing_data)
+#         # form.save()
+#         # Updating an instance in databse is possible by using ModelForm as shown above. 
+#         # IT'S JUST A DEMONSTRATION, NOT VALID WITH GIVEN DATA IN THAT EXAPMLE
         
-        form = ReviewForm(request.POST)
+#         form = ReviewForm(request.POST)
         
-        if form.is_valid():
-            # review = Review(
-            #     user_name=form.cleaned_data['user_name'], 
-            #     review_text=form.cleaned_data['review_text'], 
-            #     rating=form.cleaned_data['rating'])
-            # review.save()
-            # the code below does the same as the code above if it's ModelForm. It saves form/data in database
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+#         if form.is_valid():
+#             # review = Review(
+#             #     user_name=form.cleaned_data['user_name'], 
+#             #     review_text=form.cleaned_data['review_text'], 
+#             #     rating=form.cleaned_data['rating'])
+#             # review.save()
+#             # the code below does the same as the code above if it's ModelForm. It saves form/data in database
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
         
-    else:
-        form = ReviewForm()
+#     else:
+#         form = ReviewForm()
     
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 
 def thank_you(request):
